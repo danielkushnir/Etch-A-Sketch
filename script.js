d5493d9680e3fa749fedf05a68b13b633ccec8fd
daniel <dani2msn@gmail.com>
2022-09-23 09:51:30 -0700

    Add slider for number of squares

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.querySelector('#mainContainer').appendChild(gridContainer);
 
 
 function createRows(container) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
     for (let i = 0; i < userPrompt; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
@@ -17,7 +20,7 @@ function createGridItems(row) {
     for (let i = 0; i < userPrompt; i++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        gridItem.addEventListener('mouseover', etchRandomColor);
+        gridItem.addEventListener('mouseover', etchGrayColor);
         row.appendChild(gridItem);
     }
 }
@@ -27,13 +30,28 @@ function etchRandomColor(gridItem) {
     gridItem.target.style.backgroundColor = `#${randomColor}`;
 }
 
+function etchGrayColor(gridItem) {
+    let randomColor = Math.floor(Math.random()*256).toString(16);
+
+    gridItem.target.style.backgroundColor = `#${randomColor}${randomColor}${randomColor}`;
+}
+
 function resetGrid() {
-    userPrompt = prompt("How many squares on each side?");
-    if (userPrompt > 100) return;
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild)
-    }
+    userPrompt = 16;
     createRows(gridContainer);
+}
+
+function colorMode() {
+    createRows(gridContainer);
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach((gridItem) => gridItem.addEventListener('mouseover', etchRandomColor));
+
+}
+
+function grayscaleMode() {
+    createRows(gridContainer);
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach((gridItem) => gridItem.addEventListener('mouseover', etchGrayColor));
 }
 
 let userPrompt = 16;
@@ -41,4 +59,19 @@ createRows(gridContainer);
 
 const changeGridBtn = document.querySelector("#changeGridBtn");
 changeGridBtn.addEventListener("click", resetGrid);
+
+const colorModeBtn = document.querySelector("#colorModeBtn");
+colorModeBtn.addEventListener('click', colorMode);
+
+const grayscaleModeBtn = document.querySelector("#grayscaleModeBtn");
+grayscaleModeBtn.addEventListener('click', grayscaleMode);
+
+const gridSquareSliderLabel = document.querySelector("#gridSquareSliderLabel");
+const gridSquareSlider = document.querySelector("#gridSquareSlider");
+gridSquareSliderLabel.textContent = gridSquareSlider.value;
+gridSquareSlider.addEventListener('input', () => {
+    gridSquareSliderLabel.textContent = gridSquareSlider.value;
+    userPrompt = gridSquareSlider.value;
+    createRows(gridContainer);
+});
 
